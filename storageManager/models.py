eba7d421aadab6d492367486cefc255e1fbe6062
Ivan Gonzalez <ivdgonzalezco@unal.edu.co>
2018-09-06 19:33:30 -0500

    Productos creados, base de datos conectada
    Base de datos conectada al framework usando posgresql y python

--- storageManager/models.py
+++ storageManager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+
+    PRODUCT_STATE = (
+        ('A', 'Activate'),
+        ('D', 'Deactivate')
+    )
+
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    current_amount = models.IntegerField()
+    product_state = models.CharField(max_length=1, choices=PRODUCT_STATE)
+    registration_date = models.DateField()
+
+
--- storageManager/views.py
+++ storageManager/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.get(id__exact=1)
+    return render(request, 'storageManager/product_list.html', {'products': products})
