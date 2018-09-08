e4562a12be20ef2310298d5ed3205f43e32c088c
efberrio <efberrio@gmail.com>
2018-09-08 08:21:15 -0500

    Implementación Inicial Login y Registro de Usuarios

--- storageManager/urls.py
+++ storageManager/urls.py
@@ -12,3 +12,15 @@ urlpatterns = [
     path('supplier/new', views.supplier_new, name='supplier_new'),
     path('supplier/<int:pk>/edit/', views.supplier_edit, name='supplier_edit'),
 ]
+from django.conf.urls import include, url
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    url (r'^users/init_login', views.init_login),
+    url (r'^users/login', views.login),
+    url (r'^users/logout', views.logout),
+    path('main', views.main, name='main'),
+    url(r'^users/init_user_registration', views.init_user_registration),
+    url(r'^users/register_user', views.register_user),
+]
--- storageManager/views.py
+++ storageManager/views.py
@@ -6,6 +6,11 @@ from .models import Product, Supplier
 def products(request):
     product = Product.objects.all()
     return render(request, 'storageManager/product_list.html', {'products': product})
+from django.shortcuts import render
+from storageManager.models import User
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
 
 def product_details(request, pk):
     product = get_object_or_404(Product, pk=pk)
@@ -64,3 +69,72 @@ def supplier_edit(request, pk):
     else:
         form = SupplierForm(instance=supplier)
     return render(request, 'storageManager/supplier_new.html', {'form': form})
+
+def init_login(request, message = ""):
+    #users = User.objects.get(id__exact=1)
+    return render_to_response(
+        'storageManager/login.html',
+        {'message': message}
+    )
+
+@csrf_exempt
+def login(request):
+    errorMessage = ""
+    user_login = request.POST["user_login"]
+    user_password = request.POST["user_password"]
+
+    if len(user_login) == 0 or len(user_password) == 0:
+        errorMessage = "The login and password are required"
+    else:
+        users = User.objects.filter(login=user_login)
+        if users.count() > 0:
+            user = users[0]
+            if user.password == user_password:
+                request.session['current_user_id'] = user.id
+                request.session['current_user_name'] = user.name
+                request.session['current_user_role'] = user.user_role
+                return render(request, 'storageManager/main.html')
+            else:
+                errorMessage = "Wrong password"
+
+    return init_login(request, message=errorMessage)
+
+@csrf_exempt
+def logout(request):
+    request.session.clear()
+    return init_login(request, message="")
+
+def main(request):
+    return render_to_response(
+        'storageManager/main.html'
+    )
+
+def init_user_registration(request, message = ""):
+    users = User.objects.all()
+    return render_to_response(
+        'storageManager/register_user.html',
+        {'users': users, 'message': message}
+    )
+
+@csrf_exempt
+def register_user(request):
+    user_name = request.POST["user_name"]
+    user_login = request.POST["user_login"]
+    user_password = request.POST["user_password"]
+    user_role = request.POST["user_role"]
+    user_state = request.POST["user_state"]
+
+    if len(user_name) == 0 or len(user_login) == 0 or len(user_password) == 0 or len(user_role) == 0 or len(user_state) == 0:
+        return init_user_registration(request, message="All parameters are required. Please verify.")
+    else:
+        user = User(
+            name = user_name,
+            login = user_login,
+            password = user_password,
+            user_role = user_role,
+            user_state = user_state
+        )
+        user.save()
+
+        return init_user_registration(request, message="User saved successfully.")
+
