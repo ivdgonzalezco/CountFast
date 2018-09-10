68a8079e3bc9aab851c52c112e33544abb8319a6
efberrio <efberrio@gmail.com>
2018-09-09 23:09:44 -0500

    Se reestructura pantallas de administración de usuarios de acuerdo con nueva definición

--- storageManager/views.py
+++ storageManager/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .product_forms import ProductForm
-from .supplier_forms import SupplierForm
+from .user_forms import UserForm
+from .models import User
 from .models import Product, Supplier
-
 def products(request):
     product = Product.objects.all()
     return render(request, 'storageManager/product_list.html', {'products': product})
-from django.shortcuts import render
-from storageManager.models import User
+from .supplier_forms import SupplierForm
+from .product_forms import ProductForm
+from django.shortcuts import render, redirect, get_object_or_404
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 import re
-# Create your views here.
 
+def user_init_login(request, message = ""):
 def product_details(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'storageManager/product_details.html', {'product': product})
@@ -71,24 +71,23 @@ def supplier_edit(request, pk):
         form = SupplierForm(instance=supplier)
     return render(request, 'storageManager/supplier_new.html', {'form': form})
 
-def init_login(request, message = ""):
     return render_to_response(
         'storageManager/login.html',
         {'message': message}
     )
 
 @csrf_exempt
-def login(request):
-    user_login = request.POST["user_login"]
-    user_password = request.POST["user_password"]
+def user_login(request):
+    login = request.POST["user_login"]
+    password = request.POST["user_password"]
 
-    if len(user_login) == 0 or len(user_password) == 0:
+    if len(login) == 0 or len(password) == 0:
         error_message = "The login and password are required"
     else:
-        users = User.objects.filter(login=user_login)
-        if users.count() > 0:
-            user = users[0]
-            if user.password == user_password:
+        user_list = User.objects.filter(login=login)
+        if user_list.count() > 0:
+            user = user_list[0]
+            if user.password == password:
                 request.session['current_user_id'] = user.id
                 request.session['current_user_name'] = user.name
                 request.session['current_user_role'] = user.user_role
@@ -98,14 +97,14 @@ def login(request):
         else:
             error_message = "The user does not exist"
 
-    return init_login(request, message=error_message)
+    return user_init_login(request, message=error_message)
 
 @csrf_exempt
-def logout(request):
+def user_logout(request):
     del request.session['current_user_id']
     del request.session['current_user_name']
     del request.session['current_user_role']
-    return init_login(request, message="")
+    return user_init_login(request, message="")
 
 def main(request):
     return render_to_response(
@@ -114,69 +113,33 @@ def main(request):
          'current_user_role': request.session['current_user_role']}
     )
 
-def init_user_registration(request, message = "", user = {}):
-    users = User.objects.all()
-    return render_to_response(
-        'storageManager/register_user.html',
-        {'users': users, 'message': message, 'user': user}
-    )
+def users(request):
+    user_list = User.objects.all()
+    return render(request, 'storageManager/user_list.html', {'users': user_list})
 
-def init_user_edition(request, user_id):
-    user = User.objects.get(id = user_id)
-    return init_user_registration(request, message="", user = user)
+def user_details(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    return render(request, 'storageManager/user_details.html', {'user': user})
 
-@csrf_exempt
-def save_user(request):
-    user_id = int(request.POST["user_id"])
-    user_name = request.POST["user_name"]
-    user_login = request.POST["user_login"]
-    user_password = request.POST["user_password"]
-    user_role = request.POST["user_role"]
-    user_state = request.POST["user_state"]
-
-    if user_id > 0:
-        user = User.objects.get(id = user_id)
-        user.name = user_name
-        user.login = user_login
-        user.password = user_password
-        user.user_role = user_role
-        user.user_state = user_state
+def user_new(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            return redirect('user_details', pk=user.pk)
     else:
-        user = User(
-        name=user_name,
-        login=user_login,
-        password=user_password,
-        user_role=user_role,
-        user_state=user_state
-    )
+        form = UserForm()
+    return render(request, 'storageManager/user_new.html', {'form': form})
 
-    if len(user_name) == 0 or len(user_login) == 0 or len(user_password) == 0 or len(user_role) == 0 or len(user_state) == 0:
-        return init_user_registration(request, message="All parameters are required. Please verify.", user = user)
+def user_edit(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            return redirect('user_details', pk=user.pk)
     else:
-        if user_id > 0:
-            users = User.objects.filter(login=user_login).exclude(id = user_id)
-        else:
-            users = User.objects.filter(login=user_login)
-
-        if users.count() > 0:
-            return init_user_registration(request, message="There is already a user with the specified login.", user = user)
-
-        if not is_password_strong(user_password):
-            return init_user_registration(request, message="Password validation failed (minlength=8, min one uppercase letter, min one lowercase letter, min one digit).", user = user)
-
-        if user_id > 0:
-            user.id = user_id
-        user.save()
-
-        return init_user_registration(request, message="User saved successfully.")
-
-def is_password_strong(password):
-    length_regex = re.compile(r'.{8,}')
-    uppercase_regex = re.compile(r'[A-Z]')
-    lowercase_regex = re.compile(r'[a-z]')
-    digit_regex = re.compile(r'[0-9]')
-
-    return (length_regex.search(password) is not None
-            and uppercase_regex.search(password) is not None
-            and lowercase_regex.search(password) is not None
-            and digit_regex.search(password) is not None)
+        form = UserForm(instance=user)
+    return render(request, 'storageManager/user_new.html', {'form': form})
